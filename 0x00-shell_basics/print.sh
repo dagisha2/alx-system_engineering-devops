4610d3fa9ae7d78769ba9a6741cf1b894687daee
dagisha2 <getishdagisha@gmail.com>
2022-06-22 14:54:50 -0700

    dsgs

--- 0x00-shell_basics/print.sh
+++ 0x00-shell_basics/print.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/tmp/correction/4587112328927453779133706271455907791456_5/205/81062/0x00-shell_basics \n                                                                         
+pwd                                                                          
