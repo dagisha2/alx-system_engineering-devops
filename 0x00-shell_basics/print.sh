b4c43516fd577848ae07e6ddb804b34e3168ccc0
dagisha2 <getishdagisha@gmail.com>
2022-06-22 14:36:52 -0700

    afff

--- 0x00-shell_basics/print.sh
+++ 0x00-shell_basics/print.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo '/root/alx-system_engineering-devops/0x00-shell_basics' \n                                                                         
+/tmp/correction/4587112328927453779133706271455907791456_5/205/81062/0x00-shell_basics \n                                                                         
